50a365b4bf8421b2aa061eb60d6c882a5aca073a
JNadeu <jnadeu@gmail.com>
2018-10-23 17:36:53 +0200

    add basic.csv file to use in test_reports

--- tests/test_reports.py
+++ tests/test_reports.py
@@ -1,6 +1,9 @@
 import csv
+from io import StringIO
+from pathlib import Path
 
 import pytest
+# [(t, l) for t,l in zip(test_csv[0], list_csv[0]) if t != l]
 
 from ereuse_devicehub.client import UserClient
 from ereuse_devicehub.resources.device.models import Device
@@ -10,12 +13,26 @@ from tests.conftest import file
 
 def test_export_endpoint(user: UserClient):
     snapshot, _ = user.post(file('basic.snapshot'), res=Snapshot)
-    device_type = snapshot['device']['type']
-    csv_list, _ = user.get(res=Device, accept='text/csv')
-    read_csv = csv.reader(csv_list, delimiter=',')
-    dates = []
-    for row in read_csv:
-        date = row[0]
-        dates.append(date)
+    # device_type = snapshot['device']['type']
+    csv_str, _ = user.get(res=Device, accept='text/csv')
+    f = StringIO(csv_str)
+    obj_csv = csv.reader(f, f)
+    test_csv = list(obj_csv)
+    with Path(__file__).parent.joinpath('files').joinpath('testcsv.csv').open() as csv_file:
+        obj_csv = csv.reader(csv_file)
+        list_csv = list(obj_csv)
+        assert test_csv == list_csv, 'Csv files are different'
 
-    assert dates[1] == device_type, 'Device type are not equal'
+def test_export_empty(user: UserClient):
+    """
+    Test to check works correctly exporting csv without any information
+    """
+    pass
+
+
+def test_export_full_snaphsot(user: UserClient):
+    """
+    Test a export device with all fields
+    :return:
+    """
+    pass
