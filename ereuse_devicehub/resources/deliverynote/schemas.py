d9315c2a259586f376e082a520733ef02c872902
Big Lebowski <gmar@image.ece.ntua.gr>
2020-02-29 22:58:49 +0100

    Create, get, list collection for Deliverynote

--- ereuse_devicehub/resources/deliverynote/schemas.py
+++ ereuse_devicehub/resources/deliverynote/schemas.py
@@ -6,13 +6,15 @@ from ereuse_devicehub.resources.deliverynote import models as m
 from ereuse_devicehub.resources.user import schemas as s_user
 from ereuse_devicehub.resources.models import STR_SIZE
 from ereuse_devicehub.resources.schemas import Thing
+from ereuse_devicehub.resources.enums import TransferState
 
 
 class Deliverynote(Thing):
     id = f.UUID(dump_only=True)
-    documentID = SanitizedStr(validate=f.validate.Length(max=STR_SIZE), required=True)
+    document_id = SanitizedStr(validate=f.validate.Length(max=STR_SIZE), required=True)
     url = URL(dump_only=True, description=m.Deliverynote.url.__doc__)
     creator = NestedOn(s_user.User,only_query='id')
+    supplier_email = SanitizedStr(validate=f.validate.Length(max=STR_SIZE), required=True)
     supplier = NestedOn(s_user.User,only_query='id')
     # deposit = f.Integer(validate=f.validate.Range(min=0, max=100),
     #                    description=m.Lot.deposit.__doc__)
