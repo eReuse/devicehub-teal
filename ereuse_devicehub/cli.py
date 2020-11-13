2f761f338a670899356609b6eedeababfa7bc55d
Cayo Puigdefabregas <cayo@ribaguifi.com>
2020-11-13 13:25:51 +0100

    drop facilities in a shell

--- ereuse_devicehub/cli.py
+++ ereuse_devicehub/cli.py
@@ -7,27 +7,6 @@ import flask.cli
 from ereuse_devicehub.config import DevicehubConfig
 from ereuse_devicehub.devicehub import Devicehub
 
-import sys
-sys.ps1 = '\001\033[92m\002>>> \001\033[0m\002'
-sys.ps2= '\001\033[94m\002... \001\033[0m\002'
-
-import os, readline, rlcompleter, atexit
-history_file = os.path.join(os.environ['HOME'], '.python_history')
-try:
-  readline.read_history_file(history_file)
-except IOError:
-  pass
-readline.parse_and_bind("tab: complete")
-readline.parse_and_bind('"\e[5~": history-search-backward')
-readline.parse_and_bind('"\e[6~": history-search-forward')
-readline.parse_and_bind('"\e[5C": forward-word')
-readline.parse_and_bind('"\e[5D": backward-word')
-readline.parse_and_bind('"\e\e[C": forward-word')
-readline.parse_and_bind('"\e\e[D": backward-word')
-readline.parse_and_bind('"\e[1;5C": forward-word')
-readline.parse_and_bind('"\e[1;5D": backward-word')
-readline.set_history_length(100000)
-atexit.register(readline.write_history_file, history_file)
 
 class DevicehubGroup(flask.cli.FlaskGroup):
     # todo users cannot make cli to use a custom db this way!
