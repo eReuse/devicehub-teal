a0509a23a4fb6fc0767119475b67eb91a625de68
Santiago Lamora <santiago@ribaguifi.com>
2021-12-27 09:15:06 +0100

    Create user login view

--- ereuse_devicehub/views.py
+++ ereuse_devicehub/views.py
@@ -5,6 +5,13 @@ from flask.views import View
 core = Blueprint('core', __name__)
 
 
+class LoginView(View):
+    template_name = 'ereuse_devicehub/user_login.html'
+
+    def dispatch_request(self):
+        return render_template(self.template_name)
+
+
 class UserProfileView(View):
     template_name = 'ereuse_devicehub/user_profile.html'
 
@@ -12,4 +19,5 @@ class UserProfileView(View):
         return render_template(self.template_name)
 
 
+core.add_url_rule('/login/', view_func=LoginView.as_view('login'))
 core.add_url_rule('/profile/', view_func=UserProfileView.as_view('user-profile'))
