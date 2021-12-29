5b07c274ef576f617747f5579fad6c564eb3bd44
Cayo Puigdefabregas <cayo@ribaguifi.com>
2021-12-29 10:13:34 +0100

    drop submoduls in inventory module

--- ereuse_devicehub/inventory/views.py
+++ ereuse_devicehub/inventory/views.py
@@ -4,7 +4,6 @@ from flask.views import View
 from flask_login import login_required, current_user
 
 from ereuse_devicehub.resources.device.models import Device
-from ereuse_devicehub.utils import is_safe_url
 
 devices = Blueprint('inventory.devices', __name__, url_prefix='/inventory')
 
@@ -14,14 +13,14 @@ class DeviceListView(View):
     template_name = 'inventory/device_list.html'
 
     def dispatch_request(self):
-        # import pdb; pdb.set_trace()
+        # TODO @cayop adding filter
         filter_types = ['Desktop', 'Laptop', 'Server']
         devices = Device.query.filter(
-            Device.owner_id==current_user.id).filter(
+            Device.owner_id == current_user.id).filter(
             Device.type.in_(filter_types))
 
         context = {'devices': devices}
         return flask.render_template(self.template_name, **context)
 
 
-devices.add_url_rule('/device/list/', view_func=DeviceListView.as_view('devicelist'))
+devices.add_url_rule('/device/', view_func=DeviceListView.as_view('devicelist'))
