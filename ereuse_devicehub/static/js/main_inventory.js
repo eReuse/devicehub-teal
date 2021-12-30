94bf3aaf87e68efec97ee4ea4852dca79835f7ae
Cayo Puigdefabregas <cayo@ribaguifi.com>
2021-12-30 12:41:37 +0100

    management of the form of adding a device in a batch from the frontend

---
+++ ereuse_devicehub/static/js/main_inventory.js
@@ -0,0 +1,19 @@
+$(document) .ready(function() {
+    $(".deviceSelect").on("change", deviceSelect);
+})
+
+function deviceSelect() {
+    var devices = $(".deviceSelect").filter(':checked');
+    var devices_id = $.map(devices, function(x) { return $(x).attr('data')}).join(",");
+    if (devices_id == "") {
+        $("#addingLotModal .text-danger").show();
+        $("#addingLotModal .btn-primary").hide();
+    } else {
+        $("#addingLotModal .text-danger").hide();
+        $("#addingLotModal .btn-primary").removeClass('d-none');
+        $("#addingLotModal .btn-primary").show();
+    }
+    $.map($(".devicesList"), function(x) {
+        $(x).val(devices_id);
+    });
+}
