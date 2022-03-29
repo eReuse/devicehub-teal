34096b13eb2ac2c4c8ec41ce67fb7ed0296dc697
Cayo Puigdefabregas <cayo@ribaguifi.com>
2022-03-29 13:36:31 +0200

    clean

--- ereuse_devicehub/resources/action/views/snapshot.py
+++ ereuse_devicehub/resources/action/views/snapshot.py
@@ -11,9 +11,8 @@ from flask.json import jsonify
 from sqlalchemy.util import OrderedSet
 
 from ereuse_devicehub.db import db
-from ereuse_devicehub.parser.parser import ParseSnapshot, ParseSnapshotLsHw
-from ereuse_devicehub.resources.action.models import RateComputer, Snapshot
-from ereuse_devicehub.resources.action.rate.v1_0 import CannotRate
+from ereuse_devicehub.parser.parser import ParseSnapshotLsHw
+from ereuse_devicehub.resources.action.models import Snapshot
 from ereuse_devicehub.resources.action.schemas import Snapshot_lite
 from ereuse_devicehub.resources.device.models import Computer
 from ereuse_devicehub.resources.enums import Severity, SnapshotSoftware
