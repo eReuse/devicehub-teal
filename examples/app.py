960a4dc85f3a41c73b9358bb333194ee645a5eef
Santiago Lamora <santiago@ribaguifi.com>
2022-04-04 13:41:36 +0200

    Enable labels Blueprint on examples/app.py

--- examples/app.py
+++ examples/app.py
@@ -8,11 +8,13 @@ from flask_wtf.csrf import CSRFProtect
 from ereuse_devicehub.config import DevicehubConfig
 from ereuse_devicehub.devicehub import Devicehub
 from ereuse_devicehub.inventory.views import devices
+from ereuse_devicehub.labels.views import labels
 from ereuse_devicehub.views import core
 
 app = Devicehub(inventory=DevicehubConfig.DB_SCHEMA)
 app.register_blueprint(core)
 app.register_blueprint(devices)
+app.register_blueprint(labels)
 
 # configure & enable CSRF of Flask-WTF
 # NOTE: enable by blueprint to exclude API views
