d93f040453f3319f6bd5b225e55654753f66049c
Cayo Puigdefabregas <cayo@ribaguifi.com>
2022-07-06 18:37:27 +0200

    add placeholder log models

--- ereuse_devicehub/parser/models.py
+++ ereuse_devicehub/parser/models.py
@@ -5,6 +5,7 @@ from sqlalchemy.dialects.postgresql import UUID
 
 from ereuse_devicehub.db import db
 from ereuse_devicehub.resources.action.models import Snapshot
+from ereuse_devicehub.resources.device.models import Placeholder
 from ereuse_devicehub.resources.enums import Severity
 from ereuse_devicehub.resources.models import Thing
 from ereuse_devicehub.resources.user.models import User
@@ -41,5 +42,48 @@ class SnapshotsLog(Thing):
     def get_device(self):
         if self.snapshot:
             return self.snapshot.device.devicehub_id
+
+        return ''
+
+
+class PlaceholdersLog(Thing):
+    """A Placeholder log."""
+
+    id = Column(BigInteger, Sequence('snapshots_log_seq'), primary_key=True)
+    source = Column(CIText(), default='', nullable=True)
+    type = Column(CIText(), default='', nullable=True)
+    status = Column(CIText(), default='', nullable=True)
+
+    placeholder_id = Column(
+        UUID(as_uuid=True), db.ForeignKey(Snapshot.id), nullable=True
+    )
+    placeholder = db.relationship(
+        Snapshot, primaryjoin=placeholder_id == Placeholder.id
+    )
+    owner_id = db.Column(
+        UUID(as_uuid=True),
+        db.ForeignKey(User.id),
+        nullable=False,
+        default=lambda: g.user.id,
+    )
+    owner = db.relationship(User, primaryjoin=owner_id == User.id)
+
+    def save(self, commit=False):
+        db.session.add(self)
+
+        if commit:
+            db.session.commit()
+
+    @property
+    def phid(self):
+        if self.placeholder:
+            return self.placeholder.phid
+
+        return ''
+
+    @property
+    def dhid(self):
+        if self.placeholder:
+            return self.placeholder.device.dhid
 
         return ''
