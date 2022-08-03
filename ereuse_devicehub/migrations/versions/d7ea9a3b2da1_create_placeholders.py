1b5debc77fd0b20655fda8d966e719d62a8f7381
Cayo Puigdefabregas <cayo@ribaguifi.com>
2022-08-03 17:29:13 +0200

    fix battery migration

--- ereuse_devicehub/migrations/versions/d7ea9a3b2da1_create_placeholders.py
+++ ereuse_devicehub/migrations/versions/d7ea9a3b2da1_create_placeholders.py
@@ -78,7 +78,9 @@ def clone_device(device):
             new_c = clone_device(c)
             new_c.parent = new_device
 
-    placeholder = Placeholder(device=new_device, binding=device, is_abstract=True, owner_id=device.owner_id)
+    placeholder = Placeholder(
+        device=new_device, binding=device, is_abstract=True, owner_id=device.owner_id
+    )
     db.session.add(placeholder)
 
     tags = [x for x in device.tags]
