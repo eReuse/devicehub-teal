aee34e4989c59109454280d21f3f5636d3091315
Cayo Puigdefabregas <cayo@ribaguifi.com>
2022-09-07 11:14:13 +0200

    drop / of url

--- ereuse_devicehub/workbench/__init__.py
+++ ereuse_devicehub/workbench/__init__.py
@@ -5,7 +5,7 @@ isos = {
     },
     "2022": {
         'iso': "USODY_2022.5.2-beta.iso",
-        'url': 'http://releases.usody.com//2022/',
+        'url': 'http://releases.usody.com/2022/',
     },
     "v14": {'iso': "USODY_14.0.0.iso", 'url': 'http://releases.usody.com/v14/'},
 }
