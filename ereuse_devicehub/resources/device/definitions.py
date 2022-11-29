040421172f3af3c8cd0fa2a13e68cfd2f1765e33
Cayo Puigdefabregas <cayo@ribaguifi.com>
2022-11-29 17:46:41 +0100

    fix definition of others

--- ereuse_devicehub/resources/device/definitions.py
+++ ereuse_devicehub/resources/device/definitions.py
@@ -699,4 +699,31 @@ class ManufacturerDef(Resource):
 
 class OtherDef(DeviceDef):
     VIEW = None
+    SCHEMA = schemas.Computer
     SCHEMA = schemas.Other
+
+    def __init__(
+        self,
+        app,
+        import_name=__name__,
+        static_folder=None,
+        static_url_path=None,
+        template_folder=None,
+        url_prefix=None,
+        subdomain=None,
+        url_defaults=None,
+        root_path=None,
+        cli_commands: Iterable[Tuple[Callable, str or None]] = tuple(),
+    ):
+        super().__init__(
+            app,
+            import_name,
+            static_folder,
+            static_url_path,
+            template_folder,
+            url_prefix,
+            subdomain,
+            url_defaults,
+            root_path,
+            cli_commands,
+        )
