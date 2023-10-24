115a7cedf8821f73bdf4c666db33d8980cf8839b
pedro <git2021@cas.cat>
2023-10-24 09:46:22 +0200

    add docker support

---
+++ docker/postgres.setupdb.sql
@@ -0,0 +1,5 @@
+-- 6. Create the necessary extensions.
+CREATE EXTENSION pgcrypto SCHEMA public;
+CREATE EXTENSION ltree SCHEMA public;
+CREATE EXTENSION citext SCHEMA public;
+CREATE EXTENSION pg_trgm SCHEMA public;
