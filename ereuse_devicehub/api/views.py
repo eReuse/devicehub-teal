04d145047b4108012afebcef44faac0842f83162
Cayo Puigdefabregas <cayo@usody.com>
2024-06-23 15:19:58 +0200

    fix api snapshots lite

--- ereuse_devicehub/api/views.py
+++ ereuse_devicehub/api/views.py
@@ -12,7 +12,7 @@ from werkzeug.exceptions import Unauthorized
 from ereuse_devicehub.auth import Auth
 from ereuse_devicehub.db import db
 from ereuse_devicehub.parser.models import SnapshotsLog
-from ereuse_devicehub.parser.parser import ParseSnapshotLsHw
+from ereuse_devicehub.parser.parser import ParseSnapshot, ParseSnapshotLsHw
 from ereuse_devicehub.parser.schemas import Snapshot_lite
 from ereuse_devicehub.resources.action.views.snapshot import (
     SnapshotMixin,
@@ -51,13 +51,29 @@ class InventoryView(LoginMixin, SnapshotMixin):
         snapshot_json = json.loads(request.data)
         self.tmp_snapshots = app.config['TMP_SNAPSHOTS']
         self.path_snapshot = save_json(snapshot_json, self.tmp_snapshots, g.user.email)
-        snapshot_json = self.validate(snapshot_json)
-        if type(snapshot_json) == Response:
-            return snapshot_json
+        self.schema = Snapshot_lite()
+        try:
+            self.snapshot_json = self.schema.load(snapshot_json)
+            self.snapshot_json = ParseSnapshot(self.snapshot_json).get_snapshot()
+            snapshot = self.build()
+        except ValidationError as err:
+            txt = "{}".format(err)
+            uuid = snapshot_json.get('uuid')
+            sid = snapshot_json.get('sid')
+            version = snapshot_json.get('version')
+            error = SnapshotsLog(
+                description=txt,
+                snapshot_uuid=uuid,
+                severity=Severity.Error,
+                sid=sid,
+                version=str(version),
+            )
+            error.save(commit=True)
+            # raise err
+            self.response = jsonify(err)
+            self.response.status_code = 400
+            return self.response
 
-        self.snapshot_json = ParseSnapshotLsHw(snapshot_json).get_snapshot()
-
-        snapshot = self.build()
         snapshot.device.set_hid()
         snapshot.device.binding.device.set_hid()
         db.session.add(snapshot)
@@ -86,28 +102,6 @@ class InventoryView(LoginMixin, SnapshotMixin):
         self.response.status_code = 201
         move_json(self.tmp_snapshots, self.path_snapshot, g.user.email)
         return self.response
-
-    def validate(self, snapshot_json):
-        self.schema = Snapshot_lite()
-        try:
-            return self.schema.load(snapshot_json)
-        except ValidationError as err:
-            txt = "{}".format(err)
-            uuid = snapshot_json.get('uuid')
-            sid = snapshot_json.get('sid')
-            version = snapshot_json.get('version')
-            error = SnapshotsLog(
-                description=txt,
-                snapshot_uuid=uuid,
-                severity=Severity.Error,
-                sid=sid,
-                version=str(version),
-            )
-            error.save(commit=True)
-            # raise err
-            self.response = jsonify(err)
-            self.response.status_code = 400
-            return self.response
 
 
 api.add_url_rule('/inventory/', view_func=InventoryView.as_view('inventory'))
