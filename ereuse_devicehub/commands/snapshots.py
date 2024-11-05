d01d52d6d5754bfc303a7705ba6d446d89216e40
Cayo Puigdefabregas <cayo@puigdefabregas.eu>
2024-11-05 20:05:28 +0100

    .

--- ereuse_devicehub/commands/snapshots.py
+++ ereuse_devicehub/commands/snapshots.py
@@ -90,14 +90,67 @@ class UploadSnapshots:
             'csrf_token': generate_csrf(),
         }
 
-        self.client.post(uri, data=data, content_type="multipart/form-data", timeout=300)
+        self.client.post(uri, data=data, content_type="multipart/form-data")
 
-    def get_files(self):
-        """Read snaoshot_files dir."""
-        mypath = Path(__file__).parent.joinpath('snapshot_files')
-        for f in listdir(mypath):
-            if not isfile(join(mypath, f)):
-                continue
-            if not f[-5:] == ".json":
-                continue
-            self.onlyfiles.append(f)
+    # def get_files(self):
+    #     """Read snaoshot_files dir."""
+    #     mypath = Path(__file__).parent.joinpath('snapshot_files')
+    #     for f in listdir(mypath):
+    #         if not isfile(join(mypath, f)):
+    #             continue
+    #         if not f[-5:] == ".json":
+    #             continue
+    #         self.onlyfiles.append(f)
+
+    #     def save(self, commit=True, user_trusts=True):
+    #     if any([x == 'Error' for x in self.result.values()]):
+    #         return
+    #     schema = SnapshotSchema()
+    #     schema_lite = Snapshot_lite()
+    #     devices = []
+    #     self.tmp_snapshots = app.config['TMP_SNAPSHOTS']
+    #     for filename, snapshot_json in self.snapshots:
+    #         self.json_wb = copy.copy(snapshot_json)
+    #         path_snapshot = save_json(snapshot_json, self.tmp_snapshots, g.user.email)
+    #         debug = snapshot_json.pop('debug', None)
+    #         self.version = snapshot_json.get('schema_api')
+    #         self.uuid = snapshot_json.get('uuid')
+    #         self.sid = snapshot_json.get('sid')
+
+    #         if self.is_wb_lite_snapshot(self.version):
+    #             self.snapshot_json = schema_lite.load(snapshot_json)
+    #             snapshot_json = ParseSnapshotLsHw(self.snapshot_json).snapshot_json
+    #         else:
+    #             self.version = snapshot_json.get('version')
+    #             system_uuid = self.get_uuid(debug)
+    #             if system_uuid:
+    #                 snapshot_json['device']['system_uuid'] = system_uuid
+    #             self.get_fields_extra(debug, snapshot_json)
+
+    #         try:
+    #             snapshot_json = schema.load(snapshot_json)
+    #             response = self.build(
+    #                 snapshot_json, create_new_device=self.create_new_devices
+    #             )
+    #         except ValidationError as err:
+    #             txt = "{}".format(err)
+    #             self.errors(txt=txt)
+    #             self.result[filename] = 'Error'
+    #             continue
+
+    #         if isinstance(response.device, Computer):
+    #             response.device.user_trusts = user_trusts
+    #         db.session.add(response)
+    #         devices.append(response.device.binding.device)
+
+    #         if hasattr(response, 'type'):
+    #             self.result[filename] = 'Ok'
+    #             self.errors(txt="Ok", severity=Severity.Info, snapshot=response)
+    #         else:
+    #             self.result[filename] = 'Error'
+
+    #         move_json(self.tmp_snapshots, path_snapshot, g.user.email)
+
+    #     if commit:
+    #         db.session.commit()
+    #     return self.result, devices
